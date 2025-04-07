4467cd883502ce203f307e2c2e4f4fdc5c0774ac
Sasi Kiran Vullaganti <sasikiranvullaganti25@gmail.com>
2025-04-07 19:50:27 +0530

    paylater,refund,transaction dashboard changes

--- FinanceTrackerApplication/PayLater_App/views.py
+++ FinanceTrackerApplication/PayLater_App/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render,redirect
 from userModule_App.userValidations import checkLoginStatus 
+from userModule_App.views import getUserData
+from Transactions_App.models import PayLater
 
 # Create your views here.
 def payLaterDashboard(request):
     logincheck = checkLoginStatus(request)
     if logincheck:
         return redirect('userLogin')
-    return render(request,'PayLaterDashboard.html')
+    userid = request.session.get("user_id")
+    user = getUserData(userid)
+    paylaters = (
+        PayLater.objects.filter(transaction__user=user)
+        .select_related('transaction')
+    )
+    context = {
+        'paylaters': paylaters,
+    }
+    return render(request,'PayLaterDashboard.html',context)
--- FinanceTrackerApplication/Refunds_App/views.py
+++ FinanceTrackerApplication/Refunds_App/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render,redirect
 from userModule_App.userValidations import checkLoginStatus 
+from Transactions_App.models import Refund
+from userModule_App.views import getUserData
 
 # Create your views here.
 def refundsDashboard(request):
     logincheck = checkLoginStatus(request)
     if logincheck:
         return redirect('userLogin')
-    return render(request,'RefundsDashboard.html')
+    userid = request.session.get("user_id")
+    user = getUserData(userid)
+    refunds = (
+        Refund.objects.filter(transaction__user=user)
+        .select_related('transaction')
+    )
+    context = {
+        'refunds': refunds,
+    }
+    return render(request,'RefundsDashboard.html',context)
